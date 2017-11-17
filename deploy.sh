c24f5cace5826e03c060846015bf0f9e406b9362
Kendrick Tan <kendricktan0814@gmail.com>
2017-11-17 16:03:02 +1000

    updated zencash ver

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Push our latest revision to GitHub
+git push origin development
+
+# Clean rebuild
+yarn run build
+
+# Create deploy environment inside of .deploy directory
+mkdir .deploy
+cd .deploy
+git init
+git remote add origin git@github.com:kendricktan/kendricktan.github.io.git
+
+# Add built site files
+cp -r ../dist/* .
+echo "myzenwallet.io" >> CNAME
+git add .
+git commit -m 'Publish'
+git push -f origin master
+
+# Cleanup .deploy directory after a successful push
+cd .. && rm -rf .deploy
